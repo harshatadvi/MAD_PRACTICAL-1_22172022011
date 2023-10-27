bfb202db51ed3d411499dcc6a073f30dd85f182b
Student <student@gnu.ac.in>
2023-10-27 15:42:37 +0530

    1

---
+++ app/src/main/java/com/example/mad_practical_1_22172022011/MainActivity.kt
@@ -0,0 +1,9 @@
+android:layout_height="wrap_content"
+android:layout_marginTop="23dp"
+android:layout_marginEnd="11dp"
+android:layout_marginBottom="284dp"
+android:text="Alarm"
+app:layout_constraintBottom_toBottomOf="parent"
+app:layout_constraintEnd_toEndOf="parent"
+app:layout_constraintTop_toBottomOf="@+id/button7" />
+</androidx.constraintlayout.widget.ConstraintLayout>
